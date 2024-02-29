08690a75dede7e9553ef0b40180a777bbc8a8d41
Eshan Pandey <56771531+eshanpandey@users.noreply.github.com>
2024-02-29 11:07:54 +0000

    Create a dict and dynamic values on site using templates

--- app.py
+++ app.py
@@ -2,7 +2,34 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
- return render_template('home.html')
+ return render_template('home.html',jobs=JOBS)
+
+JOBS =[
+  {
+    'id':1,
+    'title' : 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+      'id':2,
+     'title':'Associate Software Engineer',
+     'location': 'Pune, Maharashtra',
+      'salary': 'Rs. 7,00,000'
+  },
+  {
+    'id':3,
+    'title': 'QA Engineer',
+    'location': 'Hyderabad, India',
+      'salary': 'Rs. 7,00,000'
+  },
+  {
+    'id':4,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  }
+]
 
 
 if __name__ == '__main__':
